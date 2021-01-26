61962806a1f1982a6fad0ea56df0f5df5ccc1a11
amiinqur <66859283+amiinqur@users.noreply.github.com>
2021-01-26 22:05:24 +0500

    Update script.js

--- Task 1/script.js
+++ Task 1/script.js
@@ -1261,3 +1261,14 @@ Object.prototype.clone = function () {
     }
     return newObj;
 };
+$(function(){
+  var el = document.getElementById("pacman");
+
+  if (Modernizr.canvas && Modernizr.localstorage && 
+      Modernizr.audio && (Modernizr.audio.ogg || Modernizr.audio.mp3)) {
+    window.setTimeout(function () { PACMAN.init(el, "https://raw.githubusercontent.com/daleharvey/pacman/master/"); }, 0);
+  } else { 
+    el.innerHTML = "Sorry, needs a decent browser<br /><small>" + 
+      "(firefox 3.6+, Chrome 4+, Opera 10+ and Safari 4+)</small>";
+  }
+});
